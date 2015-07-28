0eca4161b758ddfe90850bc0aee500b26129a7ec
bow <bow@bow.web.id>
2015-07-28 22:58:37 +0200

    Set sane defaults for setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,14 @@
 
 # Script for quick setup of stow-ed files
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
 type stow >/dev/null 2>&1 || { "GNU Stow not found. Exiting."; exit 1; }
 
 for prog in `find * -maxdepth 0 -type d | tail -n +2`;
 do
-    stow -d `pwd` -t ~ $prog
+    stow -d `pwd` -t ~ ${prog}
 done
