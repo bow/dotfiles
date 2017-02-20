906ba8b4ec85a9b6b280d95b3307ba64843f5c4d
bow <bow@bow.web.id>
2017-02-20 11:04:27 +0100

    Add pyenv and pyenv-virtualenv as submodules

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 type stow >/dev/null 2>&1 || { "GNU Stow not found. Exiting."; exit 1; }
 
-for prog in `find * -maxdepth 0 -type d | grep -v virtualenvwrapper | grep -v xfce`;
+for prog in `find * -maxdepth 0 -type d | grep -vP 'virtualenvwrapper|xfce|pyenv'`;
 do
     echo "Setting up ${prog} ..."
     stow -t ~ ${prog} || echo "Error when trying to set up ${prog}."
