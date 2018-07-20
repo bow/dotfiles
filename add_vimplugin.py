d9c528de7b6d750108b6068b2fe51fb6945a4076
bow <bow@bow.web.id>
2018-07-20 16:21:07 +0200

    Update vim plugin addition script

--- add_vimplugin.py
+++ add_vimplugin.py
@@ -5,7 +5,7 @@ from subprocess import check_call
 from urllib.parse import urlsplit
 
 
-def get_repo_name(git_url):
+def get_repo_name(git_url: str) -> str:
     """Returns the repo name of the given git URL."""
     sr = urlsplit(git_url)
     assert sr.scheme == "https", "this script has only been tested with " + \
@@ -16,13 +16,23 @@ def get_repo_name(git_url):
     return repo_name
 
 
-def add_vimplugin(git_url, remote_name, branch_name="master"):
-    """Sets up a Pathogen plugin using git subtree."""
+def add_vimplugin(
+        git_url: str,
+        remote_name: str,
+        path: str,
+        branch_name: str="master"
+    ) -> None:
+    """Sets up a Pathogen plugin or Vim pack using git subtree."""
     repo_name = get_repo_name(git_url)
     check_call(["git", "remote", "add", "-f", remote_name, git_url])
-    check_call(["git", "subtree", "add", "--prefix",
-                f"vim/.vim/bundle/{repo_name}", "--squash",
-                f"{remote_name}", f"{branch_name}"])
+    path = path or f"vim/.vim/bundle/{repo_name}"
+    check_call([
+        *("git subtree add --prefix".split(" ")),
+        path,
+        "--squash",
+        f"{remote_name}",
+        f"{branch_name}"
+    ])
 
 
 if __name__ == "__main__":
@@ -32,10 +42,19 @@ if __name__ == "__main__":
                         help="Plugin git repo URL.")
     parser.add_argument("-n", "--remote-name", required=True,
                         help="Name of remote.")
+    parser.add_argument("-p", "--path", required=False,
+                        default="",
+                        help="Custom path in repo at which the plugin should"
+                        " be located")
     parser.add_argument("-b", "--branch-name", required=False,
                         default="master",
                         help="Remote branch to add as subtree.")
 
     args = parser.parse_args()
 
-    add_vimplugin(args.git_url, args.remote_name, args.branch_name)
+    add_vimplugin(
+        args.git_url,
+        args.remote_name,
+        args.path,
+        args.branch_name
+    )
