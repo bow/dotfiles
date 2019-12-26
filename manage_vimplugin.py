6f1b7b329d61a3b38d37969a064f279e9a11de1d
bow <bow@bow.web.id>
2019-12-26 11:20:27 +0700

    Generalize add_vimplugin script as vim plugin management script

--- manage_vimplugin.py
+++ manage_vimplugin.py
@@ -2,6 +2,7 @@
 
 import argparse
 from subprocess import check_call
+from typing import Optional
 from urllib.parse import urlsplit
 
 
@@ -13,14 +14,15 @@ def get_repo_name(git_url: str) -> str:
     assert sr.path.endswith(".git"), "git repo url must end with '.git'"
     ptok, _ = sr.path.rsplit(".git", 1)
     _, repo_name = ptok.rsplit("/", 1)
+
     return repo_name
 
 
-def add_vimplugin(
+def add(
     git_url: str,
     remote_name: str,
-    path: str,
     branch_name: str = "master",
+    path: Optional[str] = None,
 ) -> None:
     """Set up a Pathogen plugin or Vim pack using git subtree.
 
@@ -44,27 +46,36 @@ def add_vimplugin(
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Vim plugin addition script")
-    parser.add_argument(
+    parser = argparse.ArgumentParser(
+        description="Vim plugin management script"
+    )
+    subparsers = parser.add_subparsers(dest="subcmd_name")
+
+    add_parser = subparsers.add_parser(
+        "add",
+        description="Add a new git remote to be used as a vim plugin",
+        help="Add a vim plugin",
+    )
+    add_parser.add_argument(
         "-r",
         "--git-url",
         required=True,
         help="Plugin git repo URL.",
     )
-    parser.add_argument(
+    add_parser.add_argument(
         "-n",
         "--remote-name",
         required=True,
         help="Name of remote.",
     )
-    parser.add_argument(
+    add_parser.add_argument(
         "-p",
         "--path",
         required=False,
-        default="",
+        default=None,
         help="Custom path in repo at which the plugin should be located.",
     )
-    parser.add_argument(
+    add_parser.add_argument(
         "-b",
         "--branch-name",
         required=False,
@@ -74,9 +85,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    add_vimplugin(
-        args.git_url,
-        args.remote_name,
-        args.path,
-        args.branch_name
-    )
+    if args.subcmd_name == "add":
+        add(
+            git_url=args.git_url,
+            remote_name=args.remote_name,
+            branch_name=args.branch_name,
+            path=args.path,
+        )
