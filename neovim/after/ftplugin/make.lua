f2adfbbe657bc12b52892db490ab98896e172fc6
Wibowo Arindrarto <contact@arindrarto.dev>
2021-11-25 22:59:07 +0100

    Remove vim-polyglot
    For some reason it is causing several filetypes to be incorrectly
    identified as a 'conf' type after writing.

--- neovim/after/ftplugin/make.lua
+++ neovim/after/ftplugin/make.lua
@@ -2,7 +2,3 @@ local cmd = vim.cmd
 local bufopt = vim.bo
 
 bufopt.textwidth = 100
-
-cmd [[
-  au BufWritePost Makefile,*.mk,*.make setl ft=make
-]]
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -6,7 +6,6 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', vim.fn.stdpath('data') .. '/plugged')
 
-  Plug('sheerun/vim-polyglot', {tag = 'v4.17.0'})
   Plug('neoclide/coc.nvim', {branch = 'release'})
   Plug('snakemake/snakemake', {rtp = 'misc/vim'})
   Plug('broadinstitute/vim-wdl', {commit = '1aaf200284b85d6075901faa8bda9ea67c6ce515'})
