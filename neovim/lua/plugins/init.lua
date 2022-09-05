dd96b39ef9236853bd38fc47f2b92ba76de81ee0
Wibowo Arindrarto <contact@arindrarto.dev>
2022-09-05 02:01:51 +0200

    Load neovim plugin customization using 'config' in 'use'

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -20,6 +20,11 @@ local function bootstrap()
   return cloned ~= nil
 end
 
+--- Load the named plugin configuration.
+local function load_plugin_config(name)
+  require('plugins/' .. name)
+end
+
 local bootstrapped = bootstrap()
 local packer = require('packer')
 
@@ -27,22 +32,27 @@ packer.startup(
   function(use)
     use {'wbthomason/packer.nvim'}
 
-    use {'neoclide/coc.nvim', commit = '0fd56dd25fc36606afe2290240aecb6e6ab85092'}
-    use {'vim-airline/vim-airline', tag = 'v0.11'}
+    use {
+      'neoclide/coc.nvim',
+      commit = '0fd56dd25fc36606afe2290240aecb6e6ab85092',
+      config = load_plugin_config('coc-nvim'),
+    }
+    use {
+      'vim-airline/vim-airline',
+      tag = 'v0.11',
+      config = load_plugin_config('nvim-tree'),
+    }
     use {'vim-airline/vim-airline-themes', commit = '97cf3e6e638f936187d5f6e9b5eb1bdf0a4df256'}
 
     use {'jiangmiao/auto-pairs', tag = 'v2.0.0'}
     use {'tpope/vim-commentary', commit = '627308e30639be3e2d5402808ce18690557e8292'}
     use {'tpope/vim-fugitive', tag = 'v3.6'}
-    use {'nathanaelkane/vim-indent-guides', commit = '765084d38bf102a95ab966fb06472e83fa7deff7'}
     use {'farmergreg/vim-lastplace', tag = 'v3.2.1'}
     use {'kburdett/vim-nuuid', commit = '6ae845f9348921f4e436c587da6d2bbf5691c4ed'}
     use {'kshenoy/vim-signature', commit = '6bc3dd1294a22e897f0dcf8dd72b85f350e306bc'}
-    use {'mhinz/vim-signify', tag = 'stable'}
     use {'mhinz/vim-startify', commit = '593388d3dbe7bfdcc06a714550d3253442b2fc65'}
     use {'tpope/vim-surround', commit = 'f51a26d3710629d031806305b6c8727189cd1935'}
     use {'easymotion/vim-easymotion', commit = 'd75d9591e415652b25d9e0a3669355550325263d'}
-    use {'ntpeters/vim-better-whitespace', commit = 'c5afbe91d29c5e3be81d5125ddcdc276fd1f1322'}
     use {'alvan/vim-closetag', commit = 'c0779ef575d5c239162f4ca3506cfb4a95d45a58'}
     use {'luochen1990/rainbow', commit = '54c79a24725af3a15d3aad20f70a56c7abbd46c3'}
     use {'morhetz/gruvbox', commit = '040138616bec342d5ea94d4db296f8ddca17007a'}
@@ -51,14 +61,31 @@ packer.startup(
       commit = 'a6c1d45dd6c26f7871f87564baf3860e0e5ac60c',
       requires = {
         {'kyazdani42/nvim-web-devicons', commit = 'ee101462d127ed6a5561ce9ce92bfded87d7d478'},
-      }
+      },
+      config = load_plugin_config('nvim-tree'),
     }
     use {
       'nvim-telescope/telescope.nvim',
       commit = 'd7f09f58954495d1373f3a400596b2fed71a8d1c',
       requires = {
         {'nvim-lua/plenary.nvim', commit = '96e821e8001c21bc904d3c15aa96a70c11462c5f'},
-      }
+      },
+      config = load_plugin_config('telescope-nvim'),
+    }
+    use {
+      'ntpeters/vim-better-whitespace',
+      commit = 'c5afbe91d29c5e3be81d5125ddcdc276fd1f1322',
+      config = load_plugin_config('vim-better-whitespace'),
+    }
+    use {
+      'nathanaelkane/vim-indent-guides',
+      commit = '765084d38bf102a95ab966fb06472e83fa7deff7',
+      config = load_plugin_config('vim-indent-guides'),
+    }
+    use {
+      'mhinz/vim-signify',
+      tag = 'stable',
+      config = load_plugin_config('vim-signify'),
     }
     use {
       'dstein64/vim-startuptime',
@@ -130,16 +157,3 @@ packer.startup(
     end
   end
 )
-
-for _, plugin in ipairs {
-  'telescope-nvim',
-  'vim-better-whitespace',
-  'vim-indent-guides',
-  'vim-signify',
-  'nvim-tree',
-
-  'vim-airline',
-  'coc-nvim'
-} do
-  require('plugins/' .. plugin)
-end
