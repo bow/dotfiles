3513a690f377b79544b3b82ea87ff62cea246166
Wibowo Arindrarto <contact@arindrarto.dev>
2022-10-20 08:23:25 +0200

    Add coc and autopairs integration

--- neovim/lua/plugins/coc-nvim.lua
+++ neovim/lua/plugins/coc-nvim.lua
@@ -123,3 +123,18 @@ hi CocErrorHighlight guifg=#1d2021 guibg=#cc241d gui=bold
 hi CocErrorSign guifg=#cc241d guibg=#262626 gui=none
 ]]
 
+local remap = vim.api.nvim_set_keymap
+local npairs = require('nvim-autopairs')
+npairs.setup({map_cr=false})
+
+_G.MUtils= {}
+
+MUtils.completion_confirm=function()
+    if vim.fn["coc#pum#visible"]() ~= 0  then
+        return vim.fn["coc#pum#confirm"]()
+    else
+        return npairs.autopairs_cr()
+    end
+end
+
+remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
