21e88708549ccf7043e8c992c3d7e4496983e356
Wibowo Arindrarto <contact@arindrarto.dev>
2022-11-23 05:12:26 +0100

    Remove disabled CocAction for Go files

--- neovim/after/ftplugin/go.lua
+++ neovim/after/ftplugin/go.lua
@@ -8,7 +8,3 @@ bufopt.tabstop = 4
 bufopt.textwidth = require('constants').tw.half_screen
 
 winopt.list = false
-
-cmd [[
-  au BufWritePre *.go :silent call CocAction('runCommand', 'editor.action.organizeImport')
-]]
