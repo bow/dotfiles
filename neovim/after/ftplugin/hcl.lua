8bdf8625dfff753fa8fee65160383d5f21869cc0
Wibowo Arindrarto <contact@arindrarto.dev>
2022-12-28 06:47:13 +0100

    Add custom colors for HCL files

--- neovim/after/ftplugin/hcl.lua
+++ neovim/after/ftplugin/hcl.lua
@@ -7,3 +7,16 @@ bufopt.tabstop = 2
 
 bufopt.cindent = true
 bufopt.cinoptions = bufopt.cinoptions .. ',(s,m1,+0'
+
+local tc = require('constants').gruvbox
+local u = require('utils')
+
+u.set_hls {
+  hclBlockname = {fg = tc.bright_red},
+  hclFunction = {fg = tc.bright_blue},
+  hclEscape = {fg = tc.bright_orange},
+  hclInterpolation = {fg = tc.bright_orange},
+  hclKeyword = {fg = tc.bright_blue},
+  hclString = {fg = tc.bright_green},
+  hclVariable = {fg = tc.bright_yellow},
+}
