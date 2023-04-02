3e469cd2591f9af22ab76da4175a88b6c3e2e288
Wibowo Arindrarto <contact@arindrarto.dev>
2023-04-02 11:35:22 +0200

    Update mapping for cmp confirm

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -254,7 +254,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Bslash>'] = cmp.mapping.confirm {
+    ['|'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
