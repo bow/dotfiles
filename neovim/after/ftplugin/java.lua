ba9dde0b68f136c1c58b14ea7bd0cfd057d5d4b8
Wibowo Arindrarto <contact@arindrarto.dev>
2023-04-07 08:08:27 +0200

    Remove unused CoC settings

--- neovim/after/ftplugin/java.lua
+++ neovim/after/ftplugin/java.lua
@@ -4,7 +4,3 @@ local bufopt = vim.bo
 bufopt.shiftwidth = 4
 bufopt.tabstop = 4
 bufopt.textwidth = require('constants').tw.half_screen
-
-cmd [[
-  au BufWritePre *.java :silent call CocAction('runCommand', 'editor.action.organizeImport')
-]]
