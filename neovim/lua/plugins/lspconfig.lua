3d797eef0f24a327ce89334ed2d145e1ee7ea2e7
Wibowo Arindrarto <contact@arindrarto.dev>
2023-04-07 22:05:38 +0200

    Disable luassert message

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -232,6 +232,7 @@ opt_lspconfig {
         },
         workspace = {
           library = vim.api.nvim_get_runtime_file('', true),
+          checkThirdParty = false,
         },
         telemetry = {
           enable = false,
