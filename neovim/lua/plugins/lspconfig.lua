6bf998ae98132fa7362c114cc422c950b87550e0
Wibowo Arindrarto <contact@arindrarto.dev>
2023-05-18 20:35:51 +0200

    Use '+' for cmp confirm key

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -269,7 +269,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['|'] = cmp.mapping.confirm {
+    ['+'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
