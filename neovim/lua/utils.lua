7cae381f344d20be80ad63f0ab3d7e2b76c8c44f
Wibowo Arindrarto <contact@arindrarto.dev>
2024-02-06 04:34:02 +0100

    Add nnoremap utility function for lua

--- neovim/lua/utils.lua
+++ neovim/lua/utils.lua
@@ -11,7 +11,7 @@ function M.check_exe(cmd)
   end
 end
 
---- Set nnoremap mapping.
+--- Set nnoremap mapping with nvim_set_keymap api.
 function M.nnoremap(keymap)
   local opts = keymap[3]
   local ropts = {noremap = true}
@@ -21,6 +21,18 @@ function M.nnoremap(keymap)
     for k, v in pairs(opts) do ropts[k] = v end
   end
   return api.nvim_set_keymap('n', keymap[1], keymap[2], ropts)
+end
+
+--- Set nnoremap mapping to lua functions.
+function M.nnoremapf(keymap)
+  local opts = keymap[3]
+  local ropts = {noremap = true}
+  if opts == nil then
+    ropts.unique = true
+  else
+    for k, v in pairs(opts) do ropts[k] = v end
+  end
+  return vim.keymap.set('n', keymap[1], keymap[2], ropts)
 end
 
 --- Set syntax highlighting.
