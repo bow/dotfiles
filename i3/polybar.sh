13316b949abb00fa8cd82fa013e4f91984126274
Wibowo Arindrarto <contact@arindrarto.dev>
2024-12-20 14:11:10 +0100

    Add i3 config

---
+++ i3/polybar.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar in all connected monitors
+for disp in $(xrandr | grep " connected " | awk '{ print $1 }' | sort -r)
+do
+    MONITOR="${disp}" polybar top &
+done
