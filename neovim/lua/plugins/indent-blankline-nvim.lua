c0db55860534b06d2643515b24a018224d6e28b5
Wibowo Arindrarto <contact@arindrarto.dev>
2025-05-03 00:07:06 +0200

    Update indent-blankline-nvim settings

--- neovim/lua/plugins/indent-blankline-nvim.lua
+++ neovim/lua/plugins/indent-blankline-nvim.lua
@@ -1,16 +1,13 @@
 require("ibl").setup {
   enabled = true,
   indent = {
-    char = "│",
+    char = "╎",
   },
   scope = {
     enabled = true,
     show_end = false,
-    show_exact_scope = true,
+    show_exact_scope = false,
     show_start = false,
-    highlight = { "Function", "Label" },
-    include = {
-      node_type = { ["*"] = { "*" } },
-    },
+    highlight = { "Type" },
   },
 }
