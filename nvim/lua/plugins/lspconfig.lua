f6bb5e2362002eec0c08e0fc4e89c74fc34ba1fb
Wibowo Arindrarto <contact@arindrarto.dev>
2025-10-01 05:15:44 +0200

    nvim: Add perlnavigator LSP

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -399,6 +399,16 @@ opt_lspconfig {
   },
 }
 
+opt_lspconfig {
+  name = 'perlnavigator',
+  opts = {
+    on_init = on_init,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+  },
+}
+
 local luasnip = require('luasnip')
 local cmp = require('cmp')
 
