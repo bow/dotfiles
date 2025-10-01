5dcf3c3e3521c491b07a6e651bdbba8cf65f094b
Wibowo Arindrarto <contact@arindrarto.dev>
2025-10-01 06:09:19 +0200

    nvim: Update filetype loader function to exit with a message on error

--- nvim/lua/config/filetypes/after/init.lua
+++ nvim/lua/config/filetypes/after/init.lua
@@ -2,9 +2,18 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = '*',
   callback = function()
     local ft = vim.bo.filetype
-    local ok, mod = pcall(require, 'config.filetypes.after' .. ft)
-    if ok and type(mod) == 'function' then
-      pcall(mod)
+    local load_ok, mod = pcall(require, 'config.filetypes.after.' .. ft)
+    if load_ok and type(mod) == 'function' then
+      local run_ok, rv = pcall(mod)
+      if not run_ok then
+        vim.api.nvim_echo({
+          { 'Failed to run filetype plugin for ' .. ft .. ':\n', 'ErrorMsg' },
+          { rv,                                                  'WarningMsg' },
+          { '\nPress any key to exit.' },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+      end
     end
   end,
 })
