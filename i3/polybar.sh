8ee9c0aaf0b802c799b6976e42b04cdad8273b75
Wibowo Arindrarto <contact@arindrarto.dev>
2025-10-25 14:29:49 +0200

    polybar: Update config to only accept specified monitor and network interface names

--- i3/polybar.sh
+++ i3/polybar.sh
@@ -6,7 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Get network interface names that might be shown
+wireless_if="$(ip -o link show | awk -F: '/wl|wlan/ {print $2}' | tr -d ' ')"
+eth_if="$(ip -o link show | awk -F: '/^( *[0-9]+: (en|eth))/ {print $2}' | tr -d ' ' | head -n1)"
+
 # Launch polybar in all connected monitors
-for disp in $(xrandr | grep " connected " | awk '{ print $1 }' | sort -r); do
-    MONITOR="${disp}" polybar top &
+for mon in $(xrandr | grep " connected " | awk '{ print $1 }' | sort -r); do
+    POLYBAR_MONITOR="${mon}" POLYBAR_WIRELESS_IF="${wireless_if}" POLYBAR_ETH_IF="${eth_if}" polybar top &
 done
