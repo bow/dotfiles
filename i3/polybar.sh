76426577fdd0108555324f609fce358e3fadf7c1
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-25 17:46:33 +0100

    i3: Only feed 'UP' network interfaces to polybar

--- i3/polybar.sh
+++ i3/polybar.sh
@@ -7,8 +7,8 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Get network interface names that might be shown
-wireless_if="$(ip -o link show | awk -F: '/wl|wlan/ {print $2}' | tr -d ' ')"
-eth_if="$(ip -o link show | awk -F: '/^( *[0-9]+: (en|eth))/ {print $2}' | tr -d ' ' | head -n1)"
+wireless_if="$(ip -o link show | grep ' state UP ' | awk -F: '/wl|wlan/ {print $2}' | tr -d ' ')"
+eth_if="$(ip -o link show | grep ' state UP ' | awk -F: '/^( *[0-9]+: (en|eth))/ {print $2}' | tr -d ' ' | head -n1)"
 
 # Launch polybar in all connected monitors
 for mon in $(xrandr | grep " connected " | awk '{ print $1 }' | sort -r); do
