47ea4995a8bec60b845a40fdca18072ba2ae722c
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-16 22:35:12 +0100

    nvim: Update init.lua to load optional local config

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,10 @@ require 'config.plugins'
 require 'config.options'
 require 'config.keymaps'
 require 'config.colors'
+
+local load_ok, _ = pcall(require, 'config.local')
+if load_ok then
+  vim.api.nvim_echo({ { 'Loaded local config', 'WarningMsg' } }, true, {})
+else
+  vim.api.nvim_echo({ { 'No local configs loaded', 'WarningMsg' } }, true, {})
+end
